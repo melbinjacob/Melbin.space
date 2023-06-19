176db2d70c4a398c951bc11a06939c4488cf6e3d
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-06-19 14:14:19 +0530

    completed cards section

--- js/main.js
+++ js/main.js
@@ -5,13 +5,15 @@ function navShow(){
     navLinks.style.top = "0"
 }
 function navHide(){
-    navLinks.style.top = "-100%"
+    navLinks.style.top = "-110vh"
 }
-const maxWidthQuery = window.matchMedia('(min-width: 768px)');
+const maxWidthQuery = window.matchMedia('(max-width: 768px)');
   function handleMaxWidthChange(mediaQuery) {
     if (mediaQuery.matches) {
         navHide()
+        console.log("Nav Hide")
+
     }
-  }
+}
 handleMaxWidthChange(maxWidthQuery);
 maxWidthQuery.addListener(handleMaxWidthChange);
